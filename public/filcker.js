3f440edb0731ba41ad7da8ebbfc75f1c5291575b
timerix <timerix11@gmail.com>
2022-12-02 07:16:12 +0600

    resume

---
+++ public/filcker.js
@@ -0,0 +1,20 @@
+function flicker() {
+    const elems = document.getElementsByClassName('flicker')
+    let pos = 0;
+    let id = setInterval(()=>
+        {
+            if (pos==1) {
+                for(let i=0; i<elems.length; i++){
+                    elems[i].style.color="#f0f0f0"
+                };
+                pos=0
+            }
+            else {
+                for(let i=0; i<elems.length; i++){
+                    elems[i].style.color="#ef9709"
+                };
+                pos=1
+            }
+        }
+    , 600)
+}
